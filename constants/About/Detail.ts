335c375089b66586eb54b451c5bd2cd67de42ed0
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 20:50:24 +0700

    Add: Insert logo, detail

---
+++ constants/About/Detail.ts
@@ -0,0 +1,31 @@
+import Square from "@/public/icons/About/square.png";
+import Circle from "@/public/icons/About/circle.png";
+import Triangle from "@/public/icons/About/triangle.png";
+import Star from "@/public/icons/About/star.png";
+
+export const logoDetails = [
+  {
+    index: 1,
+    icon: Square,
+    name: "Square",
+    details: "Proportion, balance and professionalism.",
+  },
+  {
+    index: 2,
+    icon: Circle,
+    name: "Circle",
+    details: "High concentration or focus.",
+  },
+  {
+    index: 3,
+    icon: Triangle,
+    name: "Triangle",
+    details: "Creativity, strength, and dynamic movement.",
+  },
+  {
+    index: 4,
+    icon: Star,
+    name: "Star",
+    details: "Light and high ideals.",
+  },
+];
