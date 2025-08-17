da4b4968858224f5720692ce9f2858dff6af4818
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 20:53:14 +0700

    Add: detail mission uf 25

---
+++ constants/About/VM.ts
@@ -0,0 +1,32 @@
+export const missions = [
+  {
+    index: 1,
+    detail:
+      "Training students of Universitas Nasional through the UNAS FEST 2024 program involves stages like Preparation (Conceptor), Planning (Conceptor), Execution (Executor), Evaluation (Evaluator), and Development (Innovator) to boost their organizational soft and hard skills.",
+  },
+  {
+    index: 2,
+    detail:
+      "Aligned with the predetermined main theme, this event aims to raise global awareness among students about environmental issues and climate change. Through this activity, the younger generation can identify and learn about renewable energy sources that are not yet fully explored or utilized in many countries.",
+  },
+  {
+    index: 3,
+    detail:
+      "This event also aims to develop students' critical, analytical, creative, innovative, adaptive, fair, and competitive thinking skills through their participation in UNAS FEST 2024. It serves as a platform for students to broaden their social connections.",
+  },
+  {
+    index: 4,
+    detail:
+      "UNAS FEST provides an opportunity for students to compete for achievements at both international and national levels.",
+  },
+  {
+    index: 5,
+    detail:
+      "The younger generation can also introduce new innovations in technology for the wise development and utilization of renewable energy.",
+  },
+  {
+    index: 6,
+    detail:
+      "The UNAS FEST committee includes members from various study programs at Universitas Nasional, providing a diverse and inclusive environment for student engagement.",
+  },
+];
