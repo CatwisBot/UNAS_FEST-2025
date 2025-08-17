126a469d13f6aef75cc4dda5b7ab4ec18512537e
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 20:56:48 +0700

    Add: image poster competition

---
+++ constants/Home/Poster.ts
@@ -0,0 +1,38 @@
+import IDC from "@/public/image/Poster/IDC.png";
+import EDC from "@/public/image/Poster/EDC.png";
+import SPC from "@/public/image/Poster/SPC.png";
+import Infografis from "@/public/image/Poster/Infografis.png";
+import Short from "@/public/image/Poster/Short-Video.png";
+
+export const AllPoster = [
+  {
+    index: 2,
+    image: IDC.src,
+    name: "Indonesia Poster",
+    path: "/activities/IDC",
+  },
+  {
+    index: 1,
+    image: EDC.src,
+    name: "English Poster",
+    path: "/activities/EDC",
+  },
+  {
+    index: 3,
+    image: SPC.src,
+    name: "SPC Poster",
+    path: "/activities/SPC",
+  },
+  {
+    index: 4,
+    image: Infografis.src,
+    name: "Infografis Poster",
+    path: "/activities/Infografis",
+  },
+  {
+    index: 5,
+    image: Short.src,
+    name: "Short Video Poster",
+    path: "/activities/Short-Video",
+  },
+];
