d044490051b2ae37b1d14640b50dfd7ff45b9f1d
NurArohman <otongrohman01@gmail.com>
2025-08-18 01:01:48 +0700

    add benefit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/shared/Home/Hero";
 import Competition from "@/components/shared/Home/Competition";
 import Ready from "@/components/shared/Home/Ready";
 import FAQ from "@/components/shared/Home/Faq";
+import Benefit from "@/components/shared/Home/Benefit";
 
 export default function homePage() {
     return(
@@ -12,8 +13,12 @@ export default function homePage() {
             {/* Competition */}
             <Competition />
 
+            {/* Benefit Section */}
+            <Benefit />
+
             {/* Ready */}
             <Ready />
+
 
             {/* FAQ */}
             <FAQ />
