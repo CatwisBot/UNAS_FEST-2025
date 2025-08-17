4b479a11fd5c8e597b074d7a4d3a0f40e318350c
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 20:56:11 +0700

    Add: image, title, desc for auto scroll

---
+++ constants/Home/Hero.ts
@@ -0,0 +1,27 @@
+import Calendar from "@/public/icons/Home/Calendar.png";
+import Medal from "@/public/icons/Home/Medal.png";
+import Person from "@/public/icons/Home/Person.png";
+import Trophy from "@/public/icons/Home/Trophy.png";
+
+export const featureHero = [
+  {
+    image: Calendar,
+    title: "Event Period",
+    detail: "July 12-20 & Aug 20-22"
+  },
+  {
+    image: Medal,
+    title: "Prize",
+    detail: "Amazing Reward"
+  },
+  {
+    image: Person,
+    title: "Participants",
+    detail: "Open Registration"
+  },
+  {
+    image: Trophy,
+    title: "Competitions",
+    detail: "Multiple Categories"
+  }
+];
