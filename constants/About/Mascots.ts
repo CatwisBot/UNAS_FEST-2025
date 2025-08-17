56e96fef997446d727298387da4cf73eaf2d6b74
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 20:51:33 +0700

    Add: Mascot Name, detail mascot

---
+++ constants/About/Mascots.ts
@@ -0,0 +1,19 @@
+import Panflora from "@/public/icons/Mascot/PANFLORA.png";
+import Daiko from "@/public/icons/Mascot/DAIKO.png";
+
+export const mascots = [
+  {
+    index: 1,
+    img: Panflora,
+    name: "Panflora & Neura",
+    description:
+      "Panflora & Neura seekor trenggiling antropomorfik yang berevolusi menjadi makhluk penjaga keharmonisan antara alam dan teknologi. Dihidupkan kembali oleh jaringan akar AI kuno, kini menjelajah bumi bersama Neura sebuah drone cerdas untuk mendeteksi kerusakan hutan, memperbaiki tanah, dan menghubungkan kehidupan alami dengan teknologi. Simbol dari tindakan pencegahan deforestasi, melalui pendekatan regeneratif berbasis teknologi dan kesadaran ekologis yang mendalam.",
+  },
+  {
+    index: 2,
+    img: Daiko,
+    name: "Daiko",
+    description:
+      'Daiko memiliki karakter penuh semangat yang terinspirasi dari burung eksotis Cendrawasih, spesies ikonik yang berasal dari Papua, Indonesia. Dikenal karena bulunya yang menawan dan gerakannya yang anggun, Cendrawasih telah lama menjadi simbol keindahan, keunikan, dan harmoni dalam alam. Penampilannya mencerminkan warna-warna asli dari burung Cendrawasih, menghadirkan kombinasi mencolok dari nuansa tropis. Tubuh utamanya berwarna kuning cerah seperti sinar matahari, melambangkan kehangatan, cahaya, dan optimisme.',
+  },
+];
