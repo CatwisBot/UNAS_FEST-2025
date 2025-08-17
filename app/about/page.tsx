adea7e260700083f50fd8ed31c789fb16c000c29
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-17 21:03:48 +0700

    Build: About us page

---
+++ app/about/page.tsx
@@ -0,0 +1,25 @@
+import AboutCarousel from "@/components/shared/About/Carousel";
+import VM from "@/components/shared/About/VM";
+import MainCommitte from "@/components/shared/About/Committee";
+import LogoSection from "@/components/shared/About/Logo";
+import LogoDetail from "@/components/shared/About/Detail";
+import OurMascot from "@/components/shared/About/Mascot";
+import Visionary from "@/components/shared/About/Visionaries";
+import PastMascots from "@/components/shared/About/Past";
+import Yearbook from  "@/components/shared/About/YB";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <AboutCarousel />
+      <VM />
+      <Yearbook/>
+      <MainCommitte />
+      <LogoSection />
+      <LogoDetail />
+      <OurMascot />
+      <PastMascots /> 
+      <Visionary />
+    </main>
+  );
+}
