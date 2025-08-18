c10d256b504824ac0bcc578b9401ce9f38d64aee
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-18 12:52:33 +0700

    Add: description poster

--- constants/Home/Poster.ts
+++ constants/Home/Poster.ts
@@ -6,33 +6,38 @@ import Short from "@/public/image/Poster/Short-Video.png";
 
 export const AllPoster = [
   {
-    index: 2,
+    index: 1,
     image: IDC.src,
     name: "Indonesia Poster",
     path: "/activities/IDC",
+    desc: "Welcome to the Indonesian Debate Competition! UNAS FEST 2025 is an event held to celebrate the 76th anniversary of Universitas Nasional. One of the competitions to be held at UNAS FEST 2025 is the Indonesian Debate Competition (IDC). The Indonesian Language Debate Competition (KDBI) is a debate competition conducted in the Indonesian language, serving as a platform for participants to present their arguments and ideas on current issues. What are you waiting for? Get ready to secure victory at UNAS FEST 2025!",
   },
   {
-    index: 1,
+    index: 2,
     image: EDC.src,
     name: "English Poster",
     path: "/activities/EDC",
+    desc: "Welcome to the English Debate Competition! UNAS FEST 2025 is an event to celebrate the 76th anniversary of Nasional University. One of the competitions that will be held at UNAS FEST 2025 is the English Debate Competition (EDC). English Debate Competition (EDC) is an English language debate competition held as a means where the participants deliver their argumentation and thoughts regarding current issues. What are you waiting for? Let’s get prepared to achieve victory in UNAS FEST 2025."
   },
   {
     index: 3,
     image: SPC.src,
     name: "SPC Poster",
     path: "/activities/SPC",
+    desc: "The UNAS FEST 2025 Scientific Paper Competition is a department that organizes a scientific paper competition based on research results using data collection methods such as observation, interviews, questionnaires, and focus group discussions (FGD). The papers are systematically structured, written in proper and correct Indonesian, and follow scientific principles that are accountable. This competition aims to produce students who are concerned about environmental and social issues and possess sharp analytical skills and critical thinking abilities. Through the process of writing and presenting scientific papers to the judging panel, participants are expected to hone their scientific communication skills, public speaking abilities, and the capacity to defend their arguments coherently.",
   },
   {
     index: 4,
     image: Infografis.src,
     name: "Infografis Poster",
     path: "/activities/Infografis",
+    desc: "The Infographics competition aims to encourage participants to think critically andcreatively, while being able to present information in a concise, accurate, and easily understandable manner. Besides honing design skills and visual message delivery, participants are also invited to increase their awareness of global issues relevant to daily life. The Infographics competition carries the theme “Conducting a Preventive Action for Deforestation Through AI-Assisted Technology Innovation in Acquiring a Resilience and Sustainable Ecosystem,” which discusses the importance of preventing deforestation by utilizing artificial intelligence (AI)-based technology to build ecosystems that are resilient to various threats and remain sustainable. The competition targets 15 teams consisting of active high school students from SMA, MAN, and SMK in the JABODETABEK area. Activities begin with a webinar to equip participants with the skills, knowledge, and understanding necessary to create their works.The Infographics competition consists of three stages: Administrative Selection, Semifinals, and Finals—all conducted online, followed by an onsite awarding ceremony.",
   },
   {
     index: 5,
     image: Short.src,
     name: "Short Video Poster",
     path: "/activities/Short-Video",
+    desc: "The Short Video competition is a contest for brief videos typically lasting from 15 seconds up to 3 minutes, designed to convey messages in a concise, creative, and informative manner. The theme, “Conducting a Preventive Action for Deforestation Through AI-Assisted Technology Innovation in Acquiring a Resilience and Sustainable Ecosystem,” addresses the importance of preventing deforestation by utilizing artificial intelligence (AI)-based technology to build ecosystems that are resilient to various threats and sustainable over time. The Short Video competition targets 15 teams consisting of active high school students from SMA, MAN, and SMK in the JABODETABEK area. The activity begins with a webinar aimed at providing participants with the skills, knowledge, and understanding needed to create effective Short Video works. The competition consists of three stages: Administrative Selection, Semifinals, and Finals — all conducted online, followed by an onsite awarding ceremony.",
   },
 ];
