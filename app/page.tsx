7fc69603ef51a02f1d1b0ff1c6a0af22449ac1e3
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-08-18 13:01:06 +0700

    add: timeline view in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/shared/Home/Hero";
 import Competition from "@/components/shared/Home/Competition";
+import Timeline from "@/components/shared/Home/Timeline";
 import Ready from "@/components/shared/Home/Ready";
 import FAQ from "@/components/shared/Home/Faq";
+import { EVENTS } from "@/constants/Activities/Timeline";
 
 export default function homePage() {
+    const homeEvents = EVENTS.find(event => event.path === "/")?.timeline;
     return(
         <main>
             {/* Hero Section */}
@@ -11,6 +14,9 @@ export default function homePage() {
 
             {/* Competition */}
             <Competition />
+
+            {/* Timeline */}
+            {homeEvents && <Timeline events={homeEvents} />}
 
             {/* Ready */}
             <Ready />
