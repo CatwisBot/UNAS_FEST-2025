9e26d18922cfb814d11c65e54ce26c4fbe76abd9
hisyamfadra-gif <hisyamfadra@gmail.com>
2025-08-20 11:47:06 +0700

    add: join with us section view

--- app/partnership/page.tsx
+++ app/partnership/page.tsx
@@ -1,9 +1,12 @@
 import Hero from "@/components/shared/Partnership/Hero"
+import Join from "@/components/shared/Partnership/Join";
+import React from "react";
 
 export default function PartnerPage () {
     return (
         <main>
             <Hero />
+            <Join />
         </main>
     )
 }
