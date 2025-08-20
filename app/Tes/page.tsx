8972e002c2466053e7a07ceaaf76922281a96231
NurArohman <otongrohman01@gmail.com>
2025-08-20 15:53:08 +0700

    rounded tes

---
+++ app/Tes/page.tsx
@@ -0,0 +1,17 @@
+import Test from "@/components/shared/Tes/Tes";
+
+export default function Baner () {
+
+    return (
+        
+        <div className="relative w-full h-screen">
+            <div className="relative w-full h-full">
+                {/* Konten Placeholder (Gambar & Logo) hanya Tampil jika video TIDAK diputar */}
+                <Test />
+            </div>
+        </div>
+
+
+    )
+
+}
