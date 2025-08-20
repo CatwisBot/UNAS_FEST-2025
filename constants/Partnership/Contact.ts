f325b6cdb9642902d85a76651b042b89cf9a25ee
hisyamfadra-gif <hisyamfadra@gmail.com>
2025-08-20 12:09:20 +0700

    add: data contact

---
+++ constants/Partnership/Contact.ts
@@ -0,0 +1,30 @@
+export const contactsData = [
+    {
+        index: 0,
+        title: "Sponsor",
+        name: "Hisyam Farindra",
+        no: "0821-1232-1577",
+        whatsApp: "https://wa.me/6282112321577",
+    },
+    {
+        index: 1,
+        title: "Media Partner",
+        name: "Ahmad Yajzid Sholeh",
+        no: "0851-8366-0257",
+        whatsApp: "https://wa.me/6285183660257",
+    },
+    {
+        index: 2,
+        title: "Donatur",
+        name: "Rivan Wahyu",
+        no: "0895-3435-9853",
+        whatsApp: "https://wa.me/62895343598535",
+    },
+    {
+        index: 3,
+        title: "Bazaar",
+        name: "Muhammad Fakhruddien",
+        no: "0877-7582-4450",
+        whatsApp: "https://wa.me/6287775824450",
+    },
+]
