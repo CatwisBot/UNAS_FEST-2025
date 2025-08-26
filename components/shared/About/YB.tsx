7bcea6587baa973f722a2a517f0a54eff239d3f7
hisyamfadra-gif <hisyamfadra@gmail.com>
2025-08-26 14:40:10 +0700

    add: button and toast yearbook

--- components/shared/About/YB.tsx
+++ components/shared/About/YB.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Image from "next/image";
-import Link from "next/link";
 import Border from "@/public/image/About/YB/Border.png";
 import Fotbar from "@/public/image/About/YB/FotoBersama.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
   return (
-    <main className="bg-gradient-to-b from-[#000022] to-[#1B1369]  from-0% to-100%">
+    <main className="bg-gradient-to-b from-[#000022] to-[#1B1369] from-0% to-100%">
       <div className="flex flex-row items-center justify-center text-center w-full max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-4 py-3 sm:py-5 md:py-10">
         <h2 className="uppercase text-xl sm:text-2xl md:text-6xl font-bold">year book</h2>
       </div>
@@ -27,6 +35,31 @@ export default function Home() {
             priority
             quality={100}
           />
+          <div className="flex items-center justify-center">
+            <button
+              onClick={handleClick}
+              className="absolute bottom-10 border border-white px-4 py-2 rounded-xl text-white font-semibold hover:scale-110 z-10 bg-transparent"
+            >
+              Visit Yearbook
+            </button>
+
+            {showModal && (
+              <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+                <div className="bg-white rounded-2xl shadow-lg p-6 w-80 text-center">
+                  <h2 className="text-lg font-bold mb-2">Notice</h2>
+                  <p className="text-black mb-4">
+                    The Yearbook for this year is not yet available.
+                  </p>
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className="px-4 py-2 bg-[#000022] text-white rounded-lg hover:bg-blue-700"
+                  >
+                    OK
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <div className="flex flex-row items-center justify-center text-center w-full max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-7xl mx-auto px-4 py-10">
