0025f83252cc1f0a4c5e424e4388fdace0251d86
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-02 15:31:34 +0700

    fix: consistants card size

--- components/ui/SponsorCard.tsx
+++ components/ui/SponsorCard.tsx
@@ -3,18 +3,21 @@ import React from "react";
 import { SponsorData } from "@/lib/types/Partnership/Sponsor";
 
 export default function SponsorCard({ sponsor }: { sponsor: SponsorData }) {
-    return (
-        <div className="flex flex-col items-center  
+  return (
+    <div
+      className="flex flex-col items-center justify-center
+            w-48 h-48
             bg-white/10 backdrop-blur-md border border-[#474242] 
             rounded-2xl shadow-lg p-4"
-        >
-            <Image
-                src={sponsor.image}
-                alt={sponsor.description}
-                width={200}
-                height={200}
-                className="rounded-lg"
-            />
-        </div>
-    );
+    >
+      <div className="relative w-full h-full flex items-center justify-center">
+        <Image
+          src={sponsor.image}
+          alt={sponsor.description}
+          fill
+          className="rounded-lg object-contain p-2"
+        />
+      </div>
+    </div>
+  );
 }
