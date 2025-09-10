5e435f893a6e292283c8493fbc27c70a4527e071
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-10 16:25:14 +0700

    fix size card

--- components/ui/MediaCard.tsx
+++ components/ui/MediaCard.tsx
@@ -4,18 +4,20 @@ import { SponsorData } from "@/lib/types/Partnership/Sponsor";
 
 export default function SponsorCard({ media }: { media: SponsorData }) {
     return (
-        <div className="flex flex-col items-center justify-center
-            w-48 h-48
-            bg-white/10 backdrop-blur-md border border-[#474242] 
-            rounded-2xl shadow-lg p-4"
-        >
-            <Image
-                src={media.image}
-                alt={media.description}
-                width={200}
-                height={200}
-                className="rounded-lg"
-            />
-        </div>
+        <div
+              className="flex flex-col items-center justify-center
+                    w-48 h-48
+                    bg-white/10 backdrop-blur-md border border-[#474242] 
+                    rounded-2xl shadow-lg p-4"
+            >
+              <div className="relative w-full h-full flex items-center justify-center">
+                <Image
+                  src={media.image}
+                  alt={media.description}
+                  fill
+                  className="rounded-lg object-contain p-2"
+                />
+              </div>
+            </div>
     );
 }
