923ef0cd26fdf0f2c63379271de2aa178aae8070
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-09-24 14:19:06 +0700

    update Visionaries data with new members and roles

--- constants/About/Visionaries.ts
+++ constants/About/Visionaries.ts
@@ -1,5 +1,15 @@
 import { StaticImageData } from 'next/image';
 
+// Web
+import Dimas from "@/public/image/About/Committee/APM/dimas.jpg";
+import Aura from "@/public/image/About/Visionaries/Web/aura.jpg";
+import Galang from "@/public/image/About/Visionaries/Web/galang.jpg";
+import Zidan from "@/public/image/About/Visionaries/Web/zidan.jpg";
+import Whisnu from "@/public/image/About/Visionaries/Web/whisnu.jpg";
+import Arohman from "@/public/image/About/Visionaries/Web/rohman.jpg";
+import Akbar from "@/public/image/About/Visionaries/Web/tama.jpg";
+import Ilham from "@/public/image/About/Visionaries/Web/ilham.jpg";
+
 import Photo from "@/public/image/About/Hero/no-photo.jpg"
 
 export interface Visionary {
@@ -21,9 +31,51 @@ export const Mascot: Visionary[] = [
 export const Web: Visionary[] = [
     {
     id: 1,
-    name: "-",
-    role: "-",
-    image: Photo,
+    name: "Dimas",
+    role: "Caturnawa Web 2025",
+    image: Dimas,
+  },
+    {
+    id: 2,
+    name: "Najwa Aura Hastin",
+    role: "UNASFEST Web 2025",
+    image: Aura,
+  },
+    {
+    id: 3,
+    name: "Galang Rispa'i",
+    role: "Design Web 2025",
+    image: Galang,
+  },
+    {
+    id: 4,
+    name: "Muhammad Zidan",
+    role: "Design Web 2025",
+    image: Zidan,
+  },
+    {
+    id: 5,
+    name: "Raden Whisnu A. N",
+    role: "UNASFEST Web 2025",
+    image: Whisnu,
+  },
+    {
+    id: 6,
+    name: "Nur Arohman",
+    role: "UNASFEST Web 2025",
+    image: Arohman,
+  },
+    {
+    id: 7,
+    name: "M. Akbar Hadi Pratama",
+    role: "Caturnawa Web 2025",
+    image: Akbar,
+  },
+    {
+    id: 8,
+    name: "Ilham Ramadhan",
+    role: "caturnawa web 2025",
+    image: Ilham,
   },
 ];
 
