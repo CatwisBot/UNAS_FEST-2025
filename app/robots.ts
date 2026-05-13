07434a5eade0c74b6d95fbdef66b0a909e7e935b
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-05-13 16:28:28 +0700

    feat: add robots.txt configuration to define crawl rules and sitemap location

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/private/', '/api/'],
+        disallow: ['/private/'],
       },
       {
         userAgent: 'Googlebot',
